73e3e6e50def0d0bf461da4fd0c2f50bc4d52c39
Alexandre Defelicibus <adefelicibus@gmail.com>
2018-11-17 15:25:35 -0200

    Fix build

--- build.sh
+++ build.sh
@@ -8,10 +8,25 @@ echo "Adding SOAD-HLA data"
 cp -r $SRC_DIR/data/ $PREFIX/share/database
 
 echo "Install SOA-HLA with Conda specific EVs"
-cp $SRC_DIR/src/* $PREFIX/bin/
 cp $SRC_DIR/include/* $PREFIX/bin/
 
-chmod 755 $PREFIX/bin/*.pl
+touch $PREFIX/bin/MHC_autopipeline
+touch $PREFIX/bin/MHC_ld2pretype_score
+touch $PREFIX/bin/MHC_sam2snp_blast
+touch $PREFIX/bin/MHC_snp2ld_assemble
+touch $PREFIX/bin/MHC_typing_construct
+
+chmod 755 $PREFIX/bin/MHC_autopipeline
+chmod 755 $PREFIX/bin/MHC_ld2pretype_score
+chmod 755 $PREFIX/bin/MHC_sam2snp_blast
+chmod 755 $PREFIX/bin/MHC_snp2ld_assemble
+chmod 755 $PREFIX/bin/MHC_typing_construct
 chmod 755 $PREFIX/bin/blastall
 chmod 755 $PREFIX/bin/formatdb
 chmod 755 $PREFIX/bin/samtools
+
+cat $SRC_DIR/src/MHC_autopipeline.pl >> $PREFIX/bin/MHC_autopipeline
+cat $SRC_DIR/src/MHC_ld2pretype_score.pl >> $PREFIX/bin/MHC_ld2pretype_score
+cat $SRC_DIR/src/MHC_sam2snp_blast.pl >> $PREFIX/bin/MHC_sam2snp_blast
+cat $SRC_DIR/src/MHC_snp2ld_assemble.pl >> $PREFIX/bin/MHC_snp2ld_assemble
+cat $SRC_DIR/src/MHC_typing_construct.pl >> $PREFIX/bin/MHC_typing_construct
